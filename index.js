9a097fb110e46e74f09321838d204ea0b13de2b0
Nafisa Ahmed <ahmednafisa11@gmail.com>
2022-11-10 12:47:55 +0600

    read operation for review done

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ async function run() {
         })
 
         //review api
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
         app.post('/reviews', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
